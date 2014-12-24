25a0545591e546f713e70b2f25b2bd1c6b5bae18
Tres Seaver <tseaver@palladion.com>
2014-12-24 17:39:36 -0500

    svb

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(*rnames):
         return f.read()
 
 setup(name='zope.principalannotation',
-      version='4.0.0',
+      version='4.0.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Annotations for Zope Principals',
