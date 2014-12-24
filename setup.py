86e66a9e76b7d6c31d17b5073ee29a20c9a0e131
Tres Seaver <tseaver@palladion.com>
2014-12-24 17:39:01 -0500

    Prep 4.0.0 release.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(*rnames):
         return f.read()
 
 setup(name='zope.principalannotation',
-      version='4.0.0.dev0',
+      version='4.0.0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Annotations for Zope Principals',
