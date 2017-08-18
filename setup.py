7de64a5cd5b0a07dad3b842aa23c7843e298cdc5
Jason Madden <jamadden@gmail.com>
2017-08-18 11:38:53 -0500

    Preparing release 4.2.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ TESTS_REQUIRE = [
 
 setup(
     name='zope.principalannotation',
-    version='4.2.0.dev0',
+    version='4.2.0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description='Annotations for Zope Principals',
