701605270a93cfef96b9e7d1fa3178b9dcb2cea7
Michael Howitz <icemac@gmx.net>
2025-09-12 08:31:50 +0200

    Switch to PEP 420 native namespace. (#17)
    * Bumped version for breaking release.
    
    * Replace ``pkg_resources`` namespace with PEP 420 native namespace.
    
    * Switch to PEP 420 native namespace.

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,8 +2,10 @@
  Changes
 =========
 
-5.2 (unreleased)
+6.0 (unreleased)
 ================
+
+- Replace ``pkg_resources`` namespace with PEP 420 native namespace.
 
 - Drop support for Python 3.8.
 
--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@
 """
 import os
 
-from setuptools import find_packages
 from setuptools import setup
 
 
@@ -28,12 +27,12 @@ TESTS_REQUIRE = [
     'zope.configuration',
     'zope.testing',
     'zope.site[test]',
-    'zope.testrunner',
+    'zope.testrunner >= 6.4',
 ]
 
 setup(
     name='zope.principalannotation',
-    version='5.2.dev0',
+    version='6.0.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.dev',
     description='Annotations for Zope Principals',
@@ -64,9 +63,6 @@ setup(
     ],
     url='https://github.com/zopefoundation/zope.principalannotation',
     license='ZPL-2.1',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    namespace_packages=['zope'],
     python_requires='>=3.9',
     extras_require={
         'test': TESTS_REQUIRE,
