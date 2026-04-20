9e464818b68310aa4ccd06ae5a3339986e4d8c31
Michael Howitz <icemac@gmx.net>
2026-04-20 09:06:29 +0200

    Move package metadata from setup.py to pyproject.toml.

--- setup.py
+++ setup.py
@@ -13,75 +13,8 @@
 ##############################################################################
 """Setup for zope.principalannotation package
 """
-import os
-
 from setuptools import setup
 
 
-def read(*rnames):
-    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
-        return f.read()
-
-
-TESTS_REQUIRE = [
-    'zope.configuration',
-    'zope.testing',
-    'zope.site[test]',
-    'zope.testrunner >= 6.4',
-]
-
-setup(
-    name='zope.principalannotation',
-    version='6.1.dev0',
-    author='Zope Foundation and Contributors',
-    author_email='zope-dev@zope.dev',
-    description='Annotations for Zope Principals',
-    long_description=(
-        read('README.rst')
-        + '\n\n' +
-        read('CHANGES.rst')
-    ),
-    keywords="zope security principal annotation",
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Zope Public License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
-        'Programming Language :: Python :: 3.14',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Topic :: Internet :: WWW/HTTP',
-        'Framework :: Zope :: 3',
-    ],
-    url='https://github.com/zopefoundation/zope.principalannotation',
-    license='ZPL-2.1',
-    python_requires='>=3.10',
-    extras_require={
-        'test': TESTS_REQUIRE,
-        'docs': [
-            'Sphinx',
-            'repoze.sphinx.autointerface',
-            'sphinx_rtd_theme',
-        ],
-    },
-    install_requires=[
-        'persistent >= 4.4.2',
-        'setuptools',
-        'zope.annotation',
-        'zope.component >= 4.5.0',
-        'zope.interface >= 4.5.0',
-        'zope.location',
-        'zope.security',
-        'zope.site',
-    ],
-    include_package_data=True,
-    zip_safe=False,
-)
+# See pyproject.toml for package metadata
+setup()
